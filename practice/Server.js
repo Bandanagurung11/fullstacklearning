aa6c5c2057a9a36db7db0edc26c4063cd34edeed
Bandanagurung11 <136729053+Bandanagurung11@users.noreply.github.com>
2025-01-23 08:46:27 +0545

    mongodb database

--- practice/Server.js
+++ practice/Server.js
@@ -1,61 +1,83 @@
 //1. import necessary modules
-import express from "express"
-import mongoose from "mongoose"
-import cors from "cors"
-
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
 
 //2. making the app
 const app = express();
 
 //3. middlewar --> every request will pass through this
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 // sabai domain bata request linx
 
-const PORT =4000;
-
-
+const PORT = 4000;
 
 //4.connecting to the mongodb database using mongoose package
 try {
-    mongoose.connect("mongodb+srv://bandanagrg881:7opLS2TM4V1uZGVV@cluster0.3wm41.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0 ");
-    console.log("MOngodb connected successfully");
+  mongoose.connect(
+    "mongodb+srv://bandanagrg881:7opLS2TM4V1uZGVV@cluster0.3wm41.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0 "
+  );
+  console.log("MOngodb connected successfully");
 } catch (error) {
-    console.log("Coundn't connect to the database.");
-    
+  console.log("Coundn't connect to the database.");
 }
 // frontend -> backend(Server/API)->database
 
 // Define Schema
-const StudendSchema= new mongoose.Schema({
-    name : { type:String, required:true},
-    age :{type: String, required:true},
-    address :{type: String, required:false},
+const StudendSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  age: { type: String, required: true },
+  address: { type: String, required: false },
 });
 // defining model, table for students
-const StudentTable=mongoose.model("StudentTable", StudendSchema);
+const StudentTable = mongoose.model("StudentTable", StudendSchema);
 //Define students route
-app.get("/students", async (req, res)=>{
+app.get("/Students", async (req, res) => {
+  try {
+    const response = await StudentTable.find();
+    return res.status(200).json(response);
+  } catch (error) {
+    console.log(
+      "Something went wrong while fetching data from students database"
+    );
+  }
+});
+
+//create student route
+app.post("/Students", async (req, res) => {
+  try {
+    console.log(req.body);
+    const newStudent = await new StudentTable(req.body).save();
+    console.log(newStudent);
+    return res.status(200).json(newStudent);
+    
+  } catch (error) {
+    console.log("Something went wrong",error);
+  }
+});
+
+
+// student delete route
+app.delete("/Students", async(req, res)=>{
     try {
-        const response = await StudentTable.find()
-        return res.status(200).json(response)
-        
+      const deletedStudent = await StudentTable.findByIdAndDelete(req.body.id)  
+      console.log(deletedStudent);
     } catch (error) {
-        console.log("Something went wrong while fetching data from students database");
+        console.log("something went wrong, error");
         
     }
 
-})
-
-// test request to the server
-app.get("/test", (req, res)=>{
-    res.status(200).json({
-        message: "Server is working",
-    });
 });
 
+// test request to the server
+app.get("/test", (req, res) => {
+  res.status(200).json({
+    message: "Server is working",
+  });
+});
 
 // run the Server
-app.listen(PORT, ()=>{
-    console.log("Server is Running");
+app.listen(PORT, () => {
+  console.log("Server is Running");
 });
